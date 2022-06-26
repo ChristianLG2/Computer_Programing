5cf8b377880bd280161380cdf4ee2656c9187ce6
Christian Lira Gonzalez <lir18001@byu.edu>
2022-06-26 11:55:34 -0600

    Mad lib created

---
+++ Programming with Blocks/Mad Libs/Mad Lib.py
@@ -0,0 +1,11 @@
+# Christian Lira's program / last edited: June 2022
+# Mad Libs are a type of funny story, where a person is asked for words without knowing their context. The words are then placed into a story in a pre-determined format, often resulting in funny statements.
+length = input("Write a length of time (exmp: '12 hrs', '20 mn', '3 Days', etc)")
+place = input("Write a random place (use articles such as: 'a','an' or 'the')")
+random_object = input("write a random object")
+terrifying_creature = input("Write a terrifying_creature (use articles such as: 'a','an' or 'the')")
+random_object2 = input("write a random object (use articles such as: 'a','an' or 'the')")
+topic_conversation = input("write a random topic of conversation")
+print()
+print("Here is your story: ")
+print(f"Is a beautiful morning, You slept {length} last night and you feel awesome and ready for a new day. as you get ready for the day you realize that you are not in your house you are in {place} and you are completely naked; so you begin to wonder how did you get there and why are you naked?! You beging to look for {random_object} to cover your nakedness. Now you need to get out of there and figure out what is happening, but suddenly{terrifying_creature} appears you know you are in danger but you see {random_object2} and decide to use it as a weapon and fight{terrifying_creature} but as you are ready to hit your first strike {terrifying_creature} begins to talk to you about {topic_conversation} You begin to remeber how much you liked talking about {topic_conversation} when you were a child but you resist to talk to {terrifying_creature} about it and finally you wake up, it was just a dream: Is a beautiful morning, You slept {length} last night and you feel awesome and ready for a new day...but you have been here before")
